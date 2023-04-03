bfe68838569cd32915c8f006e67cc3bf80bccb61
crosby7 <53411767+crosby7@users.noreply.github.com>
2023-04-03 12:13:34 -0400

    Completing HW8
    Transitioned to single page document using JQuery and eval. All other HTML pages have been commented out entirely. Nav has been updated to include ids and remove hrefs. All uploaded to Web4 server

--- README.md
+++ README.md
@@ -4,26 +4,28 @@
 
 This homework assignment will:
 
-- Teach you how to make your site responsive
-- Teach you how to deal with images next to text.
-- Use media queries
-- Use percentages
+- Teach you how to add jQuery to your page
+- Teach you how to add variables to your JS file
+- How to use variables
+- Write a function
+- Interact with HTML elements with jQuery
 
 ## Task
 
-For this homework assignment, you will be making the special offer, blog, and the contact us page from the Travel-fly prototype and making it responsive.
+For this homework assignment, you will:
 
-- You must create a project named homework-seven.
-- You must build off of the previous assignment.
-- You must fix any issues with the previous assignment, especially if it doesn't look like the prototype.
-- Your pages must look like the prototype. Your text must look good.
-- All of these pages are similar to the ones we have done earlier in the site.
-- Your page must be responsive.
-- You can align the elements as you wish but they must look good and not touch the sides of the browser.
-- Upload your project to Github and add your Web4 link to the README file.
+- You must create a project named homework-eight.
+- Create a navigation bar that has four links
+- Add content to the page when you click on a link by using jQuery
+- Use only one page and load all the content onto that page
+- All pages must have full content. Images and text on every page and professionally styled.
+- Upload your project to Github and Web4 and add your Web4 link to the README file. 
+
+
+I used the midterm as the baseline for this homework-eight. As such, there are 4 HTML pages, but only one is used in the browser (index.html). The others have all content commented out to confirm that. 
 
 Index of ~camcrosb:
 https://in-info-web4.informatics.iupui.edu/~camcrosb/
 
-Index of homework-seven:
-https://in-info-web4.informatics.iupui.edu/~camcrosb/homework-seven/index.html
+Index of homework-eight:
+https://in-info-web4.informatics.iupui.edu/~camcrosb/homework-eight/
--- homework-eight/js/app.js
+++ homework-eight/js/app.js
@@ -6,3 +6,108 @@ let homePage = `<div class="homePage backgroundImages">
 </div>
 <p class="copyright">Copyright PrettierRooms 1994</p>
 </div>`;
+
+let aboutPage = `<div class="aboutPage">
+<div class="aboutHero backgroundImages"></div>
+<div class="aboutUs">
+  <h1>About Us</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim
+    facilisis gravida neque convallis a. Tincidunt vitae semper quis
+    lectus nulla at volutpat diam. Turpis cursus in hac habitasse platea
+    dictumst quisque sagittis purus. At consectetur lorem donec massa
+    sapien faucibus et molestie ac. Euismod in pellentesque massa placerat
+    duis ultricies lacus. Ut sem viverra aliquet eget sit amet tellus cras
+    adipiscing. Nisl tincidunt eget nullam non. Commodo ullamcorper a
+    lacus vestibulum sed arcu non odio. Ut tortor pretium viverra
+    suspendisse potenti nullam ac tortor. Amet consectetur adipiscing elit
+    ut aliquam purus sit amet. Convallis convallis tellus id interdum. Est
+    pellentesque elit ullamcorper dignissim cras tincidunt lobortis.
+    Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.
+    Fermentum leo vel orci porta non pulvinar neque laoreet suspendisse.
+  </p>
+</div>
+</div>`;
+
+let blogPage = `<div class="blogPage">
+<div class="blogImages">
+  <img src="images/pexels-max-vakhtbovych-6301168.jpg" alt="" />
+  <img src="images/steven-ungermann-Aac7IlKnYX8-unsplash.jpg" alt="" />
+</div>
+<div class="theBlog">
+  <div class="blogPosts">
+    <div class="mobileImage">
+      <img src="images/pexels-max-vakhtbovych-6301168.jpg" alt="" />
+    </div>
+    <h1>Blog Entry #1:</h1>
+    <p>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Tincidunt tortor aliquam nulla facilisi. Mattis nunc sed blandit
+      libero. Consequat mauris nunc congue nisi vitae suscipit tellus.
+      Risus sed vulputate odio ut enim. Lorem dolor sed viverra ipsum nunc
+      aliquet. Enim ut tellus elementum sagittis vitae et leo duis ut.
+      Turpis egestas integer eget aliquet nibh. Id diam vel quam
+      elementum. Facilisi etiam dignissim diam quis enim lobortis
+      scelerisque fermentum. Orci nulla pellentesque dignissim enim sit
+      amet venenatis urna cursus. Massa placerat duis ultricies lacus.
+      Nullam ac tortor vitae purus."
+    </p>
+  </div>
+  <div class="blogPosts">
+    <div class="mobileImage">
+      <img src="images/steven-ungermann-Aac7IlKnYX8-unsplash.jpg" alt="" />
+    </div>
+    <h1>Blog Entry #2:</h1>
+    <p>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Tincidunt tortor aliquam nulla facilisi. Mattis nunc sed blandit
+      libero. Consequat mauris nunc congue nisi vitae suscipit tellus.
+      Risus sed vulputate odio ut enim. Lorem dolor sed viverra ipsum nunc
+      aliquet. Enim ut tellus elementum sagittis vitae et leo duis ut.
+      Turpis egestas integer eget aliquet nibh. Id diam vel quam
+      elementum. Facilisi etiam dignissim diam quis enim lobortis
+      scelerisque fermentum. Orci nulla pellentesque dignissim enim sit
+      amet venenatis urna cursus. Massa placerat duis ultricies lacus.
+      Nullam ac tortor vitae purus."
+    </p>
+  </div>
+</div>
+</div>`;
+
+let contactPage = `<div class="contactPage">
+<div class="contactHero backgroundImages"></div>
+<div class="contactUs">
+  <h1>Contact Us</h1>
+  <div class="contactForm">
+    <p>Name:</p>
+    <input type="text" name="name" id="" />
+    <p>Email:</p>
+    <input type="text" name="email" id="" />
+    <p>Phone:</p>
+    <input type="text" name="phone" id="" />
+    <p class="submit"><a href="">Submit</a></p>
+  </div>
+</div>
+</div>`;
+
+function initializeListeners() {
+  $("nav a").on("click", function (e) {
+    let clickedID = e.currentTarget.id;
+    let pageString = clickedID + "Page";
+
+    changePage(pageString);
+  });
+}
+
+function changePage(givenPage) {
+  $("#site").html(eval(givenPage));
+}
+
+$(document).ready(function() {
+  changePage("homePage");
+
+  initializeListeners();
+})
